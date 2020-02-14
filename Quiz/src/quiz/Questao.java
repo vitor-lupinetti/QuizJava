dff968fa103e49b22479f0f7acd1e3945a1ad2b1
VITOR DE OLIVEIRA LUPINETTI <082170031@ftt.cefsa.edu.br>
2020-02-13 22:36:47 -0300

    Inicial Commit

---
+++ Quiz/src/quiz/Questao.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+
+/**
+ *
+ * @author 082170031
+ */
+public class Questao {
+    private String pergunta;
+    private String alternativa1;
+    private String alternativa2;
+    private String alternativa3;
+    private String alternativa4;
+    private String alternativaCerta;
+    private String tema;
+
+    /**
+     * @return the pergunta
+     */
+    public Questao(String pergunta, String alternativa1, String alternativa2, String alternativa3,
+            String alternativa4, String alternativaCorreta, String tema){
+        this.setPergunta(pergunta);
+        this.setAlternativa1(alternativa1);
+        this.setAlternativa2(alternativa2);
+        this.setAlternativa3(alternativa3);
+        this.setAlternativa4(alternativa4);
+        this.setAlternativaCerta(alternativaCerta);
+        this.setTema(tema);
+    }
+        
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    /**
+     * @param pergunta the pergunta to set
+     */
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    /**
+     * @return the alternativa1
+     */
+    public String getAlternativa1() {
+        return alternativa1;
+    }
+
+    /**
+     * @param alternativa1 the alternativa1 to set
+     */
+    public void setAlternativa1(String alternativa1) {
+        this.alternativa1 = alternativa1;
+    }
+
+    /**
+     * @return the alternativa2
+     */
+    public String getAlternativa2() {
+        return alternativa2;
+    }
+
+    /**
+     * @param alternativa2 the alternativa2 to set
+     */
+    public void setAlternativa2(String alternativa2) {
+        this.alternativa2 = alternativa2;
+    }
+
+    /**
+     * @return the alternativa3
+     */
+    public String getAlternativa3() {
+        return alternativa3;
+    }
+
+    /**
+     * @param alternativa3 the alternativa3 to set
+     */
+    public void setAlternativa3(String alternativa3) {
+        this.alternativa3 = alternativa3;
+    }
+
+    /**
+     * @return the alternativa4
+     */
+    public String getAlternativa4() {
+        return alternativa4;
+    }
+
+    /**
+     * @param alternativa4 the alternativa4 to set
+     */
+    public void setAlternativa4(String alternativa4) {
+        this.alternativa4 = alternativa4;
+    }
+
+    /**
+     * @return the alternativaCerta
+     */
+    public String getAlternativaCerta() {
+        return alternativaCerta;
+    }
+
+    /**
+     * @param alternativaCerta the alternativaCerta to set
+     */
+    public void setAlternativaCerta(String alternativaCerta) {
+        this.alternativaCerta = alternativaCerta;
+    }
+
+    /**
+     * @return the tema
+     */
+    public String getTema() {
+        return tema;
+    }
+
+    /**
+     * @param tema the tema to set
+     */
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+    
+    
+}
---
+++ Quiz/src/quiz/Quiz.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/**
+ *
+ * @author 082170031
+ */
+public class Quiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        List<Questao> questoes = geraPerguntas();
+        
+        System.out.println("Bem vindo ao super quiz.");
+        Scanner scan = new Scanner(System.in);
+        
+       for(Questao questao : questoes){
+            System.out.println("Pergunta:");
+            System.out.println(questao.getPergunta());
+            System.out.println("A)" + questao.getAlternativa1());
+            System.out.println("B)" + questao.getAlternativa2());
+            System.out.println("C)" + questao.getAlternativa3());
+            System.out.println("D)" + questao.getAlternativa4());
+            System.out.println("Digite a alternativa que você considerar correta: ");
+            String alternativaEscolhida = scan.nextLine();
+            
+            if(questao.getAlternativaCerta().equalsIgnoreCase(alternativaEscolhida)){
+                System.out.println("Correto.");
+            }
+            else
+                System.out.println("Errado.");
+            
+        }
+        
+        System.out.println();
+        
+    }
+    
+    public static List<Questao> geraPerguntas(){
+         Questao questao1 = new Questao("Qual o nome do composto H2SO4 ?","Ácido sulfúrico","Ácido hiposulfuroso",
+                "Ácido sulfuroso","Cloreto de Sódio","Ácido sulfúrico", "Ciências");
+        
+        Questao questao2 = new Questao("Em Jogos Vorazes qual o pássaro é o símbolo do filme?", "Águia","Tordo",
+        "Pombo", "Canário","Tordo", "Filmes");
+        
+        Questao questao3 = new Questao("Ana tem 12 anos e flavia tem o dobro da idade da ana, e jorge tem o dobro da idade de flavia. Qual a idade de jorge??",
+        "48", "12", "24", "6", "48","Matemática");
+        
+        List<Questao> questoes = new ArrayList<Questao>();
+        questoes.add(questao1);
+        questoes.add(questao2);
+        questoes.add(questao3);
+        
+        return questoes;
+    }
+    
+}
