1c89df3879a8614835b6151e96dc2fe5bb747393
Vitor Lupinetti <vitorlupinetti@MacBook-Air-de-Vitor.local>
2020-02-16 21:07:38 -0300

    separacao em metodos

--- Quiz/src/quiz/Quiz.java
+++ Quiz/src/quiz/Quiz.java
@@ -22,25 +22,8 @@ public class Quiz {
         List<Questao> questoes = geraPerguntas();
         
         System.out.println("Bem vindo ao super quiz.");
-        Scanner scan = new Scanner(System.in);
         
-       for(Questao questao : questoes){
-            System.out.println("Pergunta:");
-            System.out.println(questao.getPergunta());
-            System.out.println("A)" + questao.getAlternativa1());
-            System.out.println("B)" + questao.getAlternativa2());
-            System.out.println("C)" + questao.getAlternativa3());
-            System.out.println("D)" + questao.getAlternativa4());
-            System.out.println("Digite a alternativa que você considerar correta: ");
-            String alternativaEscolhida = scan.nextLine();
-            
-            if(questao.getAlternativaCerta().equalsIgnoreCase(alternativaEscolhida)){
-                System.out.println("Correto.");
-            }
-            else
-                System.out.println("Errado");
-            
-        }
+        exibePerguntas(questoes);
         
         System.out.println();
         
@@ -62,6 +45,36 @@ public class Quiz {
         questoes.add(questao3);
         
         return questoes;
+    }
+    
+    public static void exibePerguntas(List<Questao> questoes){
+        
+        
+       for(Questao questao : questoes){
+            System.out.println("Pergunta:");
+            System.out.println(questao.getPergunta());
+            System.out.println("A)" + questao.getAlternativa1());
+            System.out.println("B)" + questao.getAlternativa2());
+            System.out.println("C)" + questao.getAlternativa3());
+            System.out.println("D)" + questao.getAlternativa4());
+            
+            analisaResposta(questao);
+            
+        }
+    }
+    
+    public static void analisaResposta(Questao questao)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a alternativa que você considerar correta: ");
+            
+            String alternativaEscolhida = scan.nextLine();
+            
+            if(questao.getAlternativaCerta().equalsIgnoreCase(alternativaEscolhida)){
+                System.out.println("Correto.");
+            }
+            else
+                System.out.println("Errado");
     }
     
 }
